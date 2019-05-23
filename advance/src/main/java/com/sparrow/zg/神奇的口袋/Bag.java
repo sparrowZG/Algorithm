341c294962d48cbb96057b20ac82768716ac7075
sparrowZG <2541108697@qq.com>
2019-05-24 00:02:31 +0800

    神奇的口袋

---
+++ advance/src/main/java/com/sparrow/zg/神奇的口袋/Bag.java
@@ -0,0 +1,21 @@
+package main.java.com.sparrow.zg.神奇的口袋;
+
+public class Bag {
+	public static void main(String[] args) {
+		int[] arr = {20, 20, 20};
+		System.out.println(countMode(arr, 0, 0));
+
+	}
+
+	public static int countMode(int[] arr, int res, int i) {
+
+		if (res == 40) {
+			return 1;
+		}
+		if (i >= arr.length) {
+			return 0;
+		}
+		return countMode(arr, res + arr[i], i + 1) + countMode(arr, res, i + 1);
+
+	}
+}
